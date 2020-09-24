abc512bf656b71d893054c4228d0aeb33d03aa66
Waqas D <wdaimee@gmail.com>
2020-09-23 22:13:41 -0400

    updated tab icon and name

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // import './App.css';
 import { Welcome } from './components/Welcome/Welcome';
 import { NavBar } from './components/Navbar/Navbar';
@@ -6,6 +6,11 @@ import { Projects } from './components/Projects/Projects';
 import { Contact } from './components/Contact/Contact';
 
 function App() {
+  // Change Tab Title Name
+  useEffect(() => {    
+    document.title = "Waqas Daimee";
+  });
+
   return (
     <div className="App">
       <NavBar />
