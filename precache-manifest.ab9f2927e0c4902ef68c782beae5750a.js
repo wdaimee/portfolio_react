77fd97f673ab452654e5c28a4bb01fa9cdf68349
Waqas D <wdaimee@gmail.com>
2020-09-24 13:07:48 -0400

    Updates

---
+++ precache-manifest.ab9f2927e0c4902ef68c782beae5750a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f1135c899732fb2f5b50f9854e545b",
+    "url": "/portfolio_react/index.html"
+  },
+  {
+    "revision": "7b5a6e93e8d668a96342",
+    "url": "/portfolio_react/static/css/main.21c336c6.chunk.css"
+  },
+  {
+    "revision": "dde4f4105615d09d240d",
+    "url": "/portfolio_react/static/js/2.6c31131b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio_react/static/js/2.6c31131b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b5a6e93e8d668a96342",
+    "url": "/portfolio_react/static/js/main.e836d3f8.chunk.js"
+  },
+  {
+    "revision": "8236a8caffc15167eaf8",
+    "url": "/portfolio_react/static/js/runtime-main.05e1413c.js"
+  },
+  {
+    "revision": "82787844d9a07c26e463d7a3137b933d",
+    "url": "/portfolio_react/static/media/Waqas Daimee Resume.82787844.pdf"
+  },
+  {
+    "revision": "c9cb3fe891c7a0604bef78c31761b577",
+    "url": "/portfolio_react/static/media/background.c9cb3fe8.jpg"
+  }
+]);
